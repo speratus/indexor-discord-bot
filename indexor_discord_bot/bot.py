7482bfb379c6cf1cb422111d3691858539dac91f
Andrew Luchuk <andrew.luchuk@outlook.com>
2023-01-14 10:03:27 -0500

    begins setting up bot to use discord.py app commands

--- indexor_discord_bot/bot.py
+++ indexor_discord_bot/bot.py
@@ -1,9 +1,19 @@
 import discord
 
+from discord import app_commands
+
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents)
+
+class IndexorClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents)
+
+        self.tree = app_commands.CommandTree(self)
+
+
+client = IndexorClient(intents)
 
 
 @client.event
